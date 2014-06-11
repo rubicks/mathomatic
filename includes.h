5068f7ce3a73c0c644fd3ffbc04eba1d85050ef8
rubicks <rubicks00@gmail.com>
2014-06-11 18:02:59 -0400

    added libtool stuff and more better testing

--- includes.h
+++ includes.h
@@ -5,24 +5,24 @@
  *
  * Copyright (C) 1987-2012 George Gesslein II.
  
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
 
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-The chief copyright holder can be contacted at gesslein@mathomatic.org, or
-George Gesslein II, P.O. Box 224, Lansing, NY  14882-0224  USA.
+ The chief copyright holder can be contacted at gesslein@mathomatic.org, or
+ George Gesslein II, P.O. Box 224, Lansing, NY  14882-0224  USA.
  
- */
+*/
 
 
 #if HAVE_CONFIG_H
